b57ff538ff68c6859510718f73518dd5331bc259
Scott Mcdermott <scott@smemsh.net>
2025-07-23 11:42:33 -0700

    - py: release: 0.7.3, mostly just file reorganization and some tmpl markers
    542a4a0 / py: imports: dirs: really should add R_OK, not just W_OK
    f68d246 / py: imports: dirs: need to import abspath
    068957c + py: imports: exe: add template moniker to exe(), not always used
    7beea09 - py: imports: traceback: move import to the site since it's catch-all
    ad4b789 - py: imports: break out sys imports by tmpl requirements
    1e11843 - py: imports: argparse: import first, but only after the version check

--- invargs.py
+++ invargs.py
@@ -4,7 +4,7 @@
 __url__     = 'https://github.com/smemsh/devskel/'
 __author__  = 'Scott Mcdermott <scott@smemsh.net>'
 __license__ = 'GPL-2.0'
-__devskel__ = '0.7.2'
+__devskel__ = '0.7.3'
 
 from sys import exit, hexversion
 if hexversion < 0x030900f0: exit("minpython: %s" % hexversion)
