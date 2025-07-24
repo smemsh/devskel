a9c20d00bf9aff678f5a3f63ebe4703f2812df9b
Scott Mcdermott <scott@smemsh.net>
2025-07-23 17:53:44 -0700

    + py: process_args: addarg: add commented-out alternate for no metavar
    sometimes we don't care if the var name is the same as the help name,
    and in these cases we'll use the shorted version of the interface.
    leave it commented out to decide during template fixation phase

--- invargs.py
+++ invargs.py
@@ -4,7 +4,7 @@
 __url__     = 'https://github.com/smemsh/devskel/'
 __author__  = 'Scott Mcdermott <scott@smemsh.net>'
 __license__ = 'GPL-2.0'
-__devskel__ = '0.7.3'
+__devskel__ = '0.7.4'
 
 from sys import exit, hexversion
 if hexversion < 0x030900f0: exit("minpython: %s" % hexversion)
@@ -79,6 +79,8 @@ def process_args():
 
     def addarg(p, vname, vdesc, help=None, /, **kwargs):
         p.add_argument(vname, metavar=vdesc, help=help, **kwargs)
+        # if no need for different name in help than code
+        #p.add_argument(vname, help=help, **kwargs)
 
     def addflag(*args, **kwargs):
         addopt(*args, action='store_true', **kwargs)
